dcd8ce14cff6d248bfc2c10d8ec6bf074ee555da
Isshin Satori <isshin.satori@gmail.com>
2017-01-24 20:14:02 +0000

    initial commit with all javascripting files

---
+++ loop-array.js
@@ -0,0 +1,5 @@
+var arr = ["cat", "dog", "rat"];
+for(var i = 0; i < arr.length; i++) {
+arr[i] = arr[i] + "s";
+}
+console.log(arr);
---
+++ strings.js
@@ -0,0 +1,2 @@
+var some_str = "this is a string";
+console.log(some_str);
